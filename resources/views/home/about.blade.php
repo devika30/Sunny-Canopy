96447f30a6e09378a38b0786cb0816c8a3ce6d2b
Abhishek Mogaveera <abmogaveer971999@gmail.com>
2019-09-28 01:42:36 +0530

    small edit to about

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -32,14 +32,14 @@
             
                 <div class="col-3 text-center" style="border:1px solid #DCDCDC">
                     <img class="mb-3" style="border:4px solid blue;height:125px;border-radius: 50%;margin-top:-70px;" src="https://www.21cmuseumhotels.com/kansascity/wp-content/uploads/sites/17/2019/07/Spencer-Knipper-Urban-Table.jpg">
-                    <h4>Joyce Bowman</h4>
+                    <h4>Peter Hart</h4>
                     <small class="text-muted">CHEF</small>
                     <p class="text-muted">Donec porta eleifend mauris ut effici-tur. Quisque non velit vestibulum, lob-ortis mi eget, rhoncus nunc</p>
                     
                 </div>
                 <div class="col-3 text-center offset-1" style="border:1px solid #DCDCDC">
                         <img class="mb-3" style="border:4px solid blue;height:125px;border-radius: 50%;margin-top:-70px;" src="https://images.squarespace-cdn.com/content/v1/56c3b16f4d088e0187ca1f96/1508788576605-MNYP1DOBTKJ9OTGC1WBR/ke17ZwdGBToddI8pDm48kMQ36V1L8azCx0lJDQhf1klZw-zPPgdn4jUwVcJE1ZvWEtT5uBSRWt4vQZAgTJucoTqqXjS3CfNDSuuf31e0tVFANmGTW5-M4fv__c2oaXMhuKbvltorCn9t2WE8fCyjiMYRvXol31x0F6DPQ-7cIyY/DSC_1924.jpg">
-                        <h4>Peter Hart</h4>
+                        <h4>Joyce Bowman</h4>
                         <small class="text-muted">CHEF</small>
                         <p class="text-muted">Donec porta eleifend mauris ut effici-tur. Quisque non velit vestibulum, lob-ortis mi eget, rhoncus nunc</p>
                         
@@ -60,22 +60,43 @@
                 </div>
                 <div class="col-3">
                     <img src="https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" style="
-                    margin-top: -10px;
-                    margin-left: 10px;height:400px;">
+                    margin-top: -25px;
+                    margin-left: 15px;height:400px;">
                 </div>
                 <div class="col-3">
                     <img src="https://images.unsplash.com/photo-1482049016688-2d3e1b311543?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=353&q=80" style="
-                    margin-top: 10px;
-                    margin-left: -10px;height:400px;">
+                    margin-top: 15px;
+                    margin-left: -15px;height:400px;">
                 
                 </div>
                 <div class="col-3">
                         <img src="https://images.unsplash.com/photo-1476718406336-bb5a9690ee2a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" style="
                         margin-top: -25px;
-                        margin-left:-15px;height:400px;">
+                        margin-left:-25px;height:400px;">
                 </div>
             </div>
-            
+            <div class="row justify-content-center" style="margin-top:100px;margin-bottom:100px;">
+                    <div class="col-3">
+                        <img src="https://images.unsplash.com/photo-1566550747935-21b7f2464b57?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1575&q=80" style="
+                        margin-top: -25px;
+                        margin-left: 15px;height:400px;">
+                    </div>
+                    <div class="col-3">
+                        <img src="https://images.unsplash.com/photo-1551628335-d9861f3b05ae?ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80" style="
+                        margin-top: 15px;
+                        margin-left: -25px;height:400px;">
+                    
+                    </div>
+                    <div class="col-3">
+                            <img src="https://images.unsplash.com/photo-1555352660-d09491a69d54?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=356&q=80" style="
+                            margin-top: -25px;
+                            margin-left:-50px;height:400px;">
+                    </div>
+                    <div class="col-3">
+                            <h2 style="color:crimson;font-size:50px;font-family: 'Dancing Script', cursive;">Amazing Rooms, Amazing View</h2>
+                        <p>Suscipit libero pretium nullam potenti. Interdum, blandit phasellus consectetuer dolor ornare dapibus enim ut tincidunt rhoncus tellus sollicitudin pede nam maecenas, dolor sem. Neque sollicitudin enim. Dapibus lorem feugiat facilisi faucibus et. Rhoncus.</p>
+                    </div>
+                </div>
             
             <div class="container">
                     <div class="row justify-content-center">
